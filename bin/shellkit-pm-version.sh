cd6b064360292da1e782db8c4423e72193eece0c
Les Matheson <les.matheson@gmail.com>
2022-08-27 07:11:01 -0400

    Bump to 0.4.3

--- bin/shellkit-pm-version.sh
+++ bin/shellkit-pm-version.sh
@@ -2,7 +2,7 @@
 
 # Running shellkit-pm-version.sh is the correct way to
 # get the home install path for the tool
-KitVersion=0.4.2
+KitVersion=0.4.3
 
 canonpath() {
     # Like "readlink -f", but portable
