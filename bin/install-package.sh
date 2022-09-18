97ce7f87cffd13635e1c4ccd942a818ddca0deba
Les Matheson <les.matheson@gmail.com>
2022-09-18 06:09:59 -0400

    Sync (auto)

--- bin/install-package.sh
+++ bin/install-package.sh
@@ -32,6 +32,8 @@ die() {
     fi
 }
 
+mode_direct=1
+mode_indirect=2
 
 _query_package() {
     ${scriptDir}/shellkit-query-package.sh "$@"
@@ -48,11 +50,34 @@ stub() {
 }
 
 _parse_setupscript_uri() {
-    local pkgName="$1"
-    local html_file="$2"
-    local tx1=$(command grep -Eo "\".*releases.*${pkgName}-setup-[^ ]+" ${html_file})
-    [[ -n $tx1 ]] || return $(die "Failed to parse setup script URL from $html_file")
-    echo "$tx1" | command tr -d '"'
+    local mode="$1"
+    local pkgName="$2"
+    local html_file="$3"
+    case $mode in
+        mode_direct)
+            # Sometimes the release page contains the list of release assets embedded in it
+            # (mode_direct):
+            local tx1=$(command grep -Eo "\".*releases.*${pkgName}-setup-[^ ]+" ${html_file})
+            [[ -n $tx1 ]] || {
+                return $(echo "Can't parse setup script URL from $html_file" >&2; exit 1;)
+            }
+            builtin echo "$tx1" | command tr -d '"'
+            return
+            ;;
+        mode_indirect)
+            # Sometimes the release page is built async, and the actual list of assets requires
+            # a separate curl fetch
+            stub "${FUNCNAME[0]}.${LINENO}" "$@" "mode_indirect"
+            local tx2=$(command grep -Eo 'https://.*github.*expanded_assets[^"]*' ${html_file})
+            [[ -n $tx2 ]] || {
+                return $(die "Failed to parse expanded_assets URL from $html_file")
+            }
+            builtin echo "$tx2"
+            ;;
+        *)
+            die 209.2
+            ;;
+    esac
 }
 
 _get_base_url_from_canon_source() {
@@ -76,20 +101,34 @@ _download_github_release() {
     local tmpdir=$(command mktemp -d)
     (
         cd ${tmpdir} || die "201.3"
+        stub "${FUNCNAME[0]}.${LINENO}" "$@" "download-prep"
         command curl $(curl_opts) "${canon_source}/releases/${version}" > rawpage.html
         [[ $? -eq 0 ]] || {
             echo "Failed to retrieve raw html" >&2; false;
             return;
         }
-        uri=$(_parse_setupscript_uri ${pkgName} "$PWD/rawpage.html")
-        [[ -n $uri ]] || return $(die "download failed 102.4")
+        uri=$(_parse_setupscript_uri mode_direct ${pkgName} "$PWD/rawpage.html")
+        [[ -n $uri ]] || {
+            # Maybe we should be looking for the expanded-assets URL ("mode_indirect")?
+            echo "Trying indirect asset list fetch for ${pkgName}" >&2
+            uri=$(_parse_setupscript_uri mode_indirect ${pkgName} "$PWD/rawpage.html")
+            [[ -n $uri ]] || {
+                return $(die "download failed 102.4 ")
+            }
+            # We got a url for the expanded-assets chunk: this should end with the coveted version number:
+            local actual_version=$(basename $uri)
+            stub "${FUNCNAME[0]}.${LINENO}" $actual_version $uri $canon_source
+            uri="/releases/download/${actual_version}/${pkgName}-setup-${actual_version}.sh"
+        }
         base_url=$( _get_base_url_from_canon_source "${canon_source}" )
+        stub "${FUNCNAME[0]}.${LINENO}" "base-url" "$base_url" "uri" "$uri"
         full_url="${base_url}${uri}"
         dest_file="${PWD}/${pkgName}-setup-${version}.sh"
         command curl $(curl_opts)  "$full_url" > "${dest_file}"
         [[ $? -eq 0 ]] || return $(die "Failed downloading $full_url")
         chmod +x "$dest_file"
         echo "$dest_file"
+        stub "${FUNCNAME[0]}.${LINENO}" "setup script downloaded" "$dest_file"
     )
 }
 
