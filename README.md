711f0f40cef86320d99361f5378a303222a0d1c4
Les Matheson <lmatheson4@bloomberg.net>
2022-12-06 03:17:59 -0500

    Overwrite packages metadata unconditionally

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Package manager for shellkit-based tools
 
 ## Setup
-1. Download and run the [shellkit bootstrap script](https://github.com/sanekits/shellkit-pm/releases/download/0.7.2/shellkit-bootstrap.sh)
+1. Download and run the [shellkit bootstrap script](https://github.com/sanekits/shellkit-pm/releases/download/0.8.0/shellkit-bootstrap.sh)
 2. Restart your shell (`bash -l`) and use `shpm list` to display the available packages
 3. Choose one or more packages and pass the names to `shpm install <package> [...<package>]`
 
--- bin/shellkit-bootstrap.sh
+++ bin/shellkit-bootstrap.sh
@@ -2,7 +2,7 @@
 # shellkit-bootstrap.sh:  starting from pure scratch, install current shellkit-meta + shellkit-pm
 #  Provides intro guidance on kit setup
 
-shellkitpm_version=0.7.2  # Initial bootstrap version.  You can always do `shpm install shellkit-pm` to update it
+shellkitpm_version=0.8.0  # Initial bootstrap version.  You can always do `shpm install shellkit-pm` to update it
 
 canonpath() {
     ( cd -L -- "$(command dirname -- ${1})"; echo "$(command pwd -P)/$(command basename -- ${1})" )
@@ -72,20 +72,7 @@ main() {
             builtin echo "${setup_script}: OK"
         fi
         command mkdir -p ~/.config/shellkit-meta
-        local overwrite_packages=true
-        echo
-        if [[ -f ~/.config/shellkit-meta/packages ]]; then
-            # A zero-byte packages file will get overwritten.  If it has real content, we'll protect it.
-            if [[ $(stat --printf="%s" ~/.config/shellkit-meta/packages) -gt 0 ]]; then
-                command diff ~/.config/shellkit-meta/packages ./packages &>/dev/null || {
-                    overwrite_packages=false
-                    command cp ./packages ~/.config/shellkit-meta/packages.proposed || die "Failed copying 'packages' to ~/.config/shellkit-meta/packages.proposed from $PWD"
-                    echo -e "WARNING: you already have a ~/.config/shellkit-meta/packages file.  I didn't overwrite it, but you'll find 'packages.proposed' in that same directory.  You should compare the two and manually merge the changes that you want, or delete your old 'packages' and re-run ${scriptName}" | fold -s >&2
-                }
-            fi
-        fi
-        $overwrite_packages && \
-            command cp ./packages ~/.config/shellkit-meta/packages || die "Failed copying 'packages' to ~/.config/shellkit-meta/ from $PWD"
+        command cp ./packages ~/.config/shellkit-meta/packages || die "Failed copying 'packages' to ~/.config/shellkit-meta/ from $PWD"
         echo -e  $'\n' \
             " 1.  Restart your shell with \"bash -l\"" $'\n' \
             " 2.  To see the available packages, run \"shpm list\"" $'\n' \
